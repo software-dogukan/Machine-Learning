ac0bdc5891ba4728f272433a84f2534d50b2f788
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-10-05 15:00:06 +0300

    Data Merging

---
+++ data_merging.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn import preprocessing
+data=pd.read_csv("name.csv")
+new_data=data.iloc[:,0:1].values#forexample it is text
+le=preprocessing.LabelEncoder()
+new_data[:,0]=le.fit_transform(data.iloc[:,0])
+ohe=preprocessing.OneHotEncoder()
+new_data=ohe.fit_transform(new_data).toarry()
+data2=pd.concat(new_data,data[:,1:], axis=1)

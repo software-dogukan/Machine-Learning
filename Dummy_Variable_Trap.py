5d8b12ae4e3173617a94c771b60d077b4be2bfd2
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-10-14 22:54:35 +0300

    Update Dummy_Variable_Trap.py

--- Dummy_Variable_Trap.py
+++ Dummy_Variable_Trap.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import tain_test_split
 from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor2=LinearRegression()
 le=preprocessing.LabelEncoder()
 ohe=preprocessing.OneHotEncoder()
 data=pd.read_csv("name.csv")
@@ -18,3 +21,13 @@ combination2=pd.concat([combination1,finally3[:,0:1]],axis=1)
 for_x=pd.DataFrame(data=combination1,index=range(22),columns=["Tr","Fr","Us","age","height","weight"])
 for_y=pd.DataFrame(data=data[:,-1],index=range(22),columns=["Gender"])
 x_train,x_test,y_train,y_test=tain_test_split(for_x,for_y,test_size=0.33,random_state=0)
+regressor.fit(x_train,y_train)
+y_pred=regressor.predict(x_test)
+#example 2
+weight=combination2.iloc[:,3:4].values
+left=combination2.iloc[:,:3]
+right=combination2.iloc[:,4:]
+data=pd.concat([left,right],axis=1)
+x_train,x_test,y_train,y_test=tain_test_split(data,weight,test_size=0.33,random_state=0)
+regressor2.fit(x_train,y_train)
+y_pred=regressor2.predict(x_test)
